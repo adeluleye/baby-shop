c0b23ab5d6a53bf1901e42cacd6b4b8d96b3be4b
adeluleye <adelu4real@gmail.com>
2018-08-20 17:27:29 +0100

    Implement Logout, add username to navbar

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -6,11 +8,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
+  user: firebase.User;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user => this.user = user);
+  }
 
-  ngOnInit() {
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
