da4dae1f85d78ac792bc7f0f1bf9d586ec30d835
adeluleye <adelu4real@gmail.com>
2018-08-20 18:35:40 +0100

    Protect admin page, check-out, orders from Anonymous users

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,28 @@
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private auth: AuthService, private router: Router) { }
+
+  canActivate() {
+    return this.auth.user$
+      .pipe(
+        map(user => {
+          if (user) {
+            return true;
+          }
+
+          this.router.navigate(['/login']);
+          return false;
+        })
+      );
+
+  }
+}
