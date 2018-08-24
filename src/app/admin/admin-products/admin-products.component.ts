5dfd4d2bfce580533ce1bc47c900f5287cd83a6d
adeluleye <adelu4real@gmail.com>
2018-08-24 12:17:56 +0100

    Display list of products from firebase in a bootstrap table

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../product.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin-products',
@@ -6,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit {
+  products$;
 
-  constructor() { }
+  constructor(private productService: ProductService) {
+    // this.products$ = this.productService.getAll();
+    this.products$ = this.productService.getAll().snapshotChanges().pipe(
+      map(products =>
+        products.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
+  }
 
   ngOnInit() {
   }
