f7020d74c99f52530b96cddb0bc65195597431ed
adeluleye <adelu4real@gmail.com>
2018-08-25 17:35:08 +0100

    Remove angular-4-data-table

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -4,26 +4,54 @@ import { ProductService } from '../../product.service';
 import { map } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
 
+
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
-  products: Product[];
+  products: Product[] = [];
   filteredProducts: any[];
   subscription: Subscription;
+
+  // tableResource: DataTableResource<Product>;
+  // items: Product[];
+  // itemCount: number;
 
   constructor(private productService: ProductService) {
     // this.products$ = this.productService.getAll();
     this.subscription = this.productService.getAll().snapshotChanges()
       .pipe(
         map(products =>
-          products.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+          products.map(c => ({
+            key: c.payload.key,
+            ...c.payload.val() }))
         )
       )
-      .subscribe(products => this.filteredProducts = this.products = products);
+      .subscribe(products => {
+        this.filteredProducts = this.products = products;
+        // this.initializeTable(products);
+
+      });
   }
+
+  // private initializeTable(products: Product[]) {
+  //   this.tableResource = new DataTableResource(products);
+  //   this.tableResource.query({ offset: 0 })
+  //     .then(items => this.items = items);
+  //   this.tableResource.count()
+  //     .then(count => this.itemCount = count);
+  // }
+
+  // reloadItems(params) {
+  //   if (!this.tableResource) {
+  //     return;
+  //   }
+
+  //   this.tableResource.query(params)
+  //     .then(items => this.items = items);
+  // }
 
   filter(query: string) {
     // console.log(query);
