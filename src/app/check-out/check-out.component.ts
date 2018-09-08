b43a6f2a9e7ab9951fc3f8b934dcf7d4c830e497
adeluleye <adelu4real@gmail.com>
2018-09-08 18:11:35 +0100

    Refactor checkout, create shipping form & shopping-cart-summary components

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,46 +1,21 @@
-import { AuthService } from './../auth.service';
-import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ShoppingCart } from './../models/shopping-cart';
 import { ShoppingCartService } from './../shopping-cart.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { OrderService } from '../order.service';
-import { Order } from '../models/order';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-check-out',
+  // tslint:disable-next-line:component-selector
+  selector: 'check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit, OnDestroy {
+export class CheckOutComponent implements OnInit {
+  cart$: Observable<ShoppingCart>;
 
-  shipping = {};
-  cart: ShoppingCart;
-  userId: string;
-  cartSubscription: Subscription;
-  userSubscription: Subscription;
-
-  constructor(
-    private router: Router,
-    private authService: AuthService,
-    private orderService: OrderService,
-    private shoppingCartService: ShoppingCartService) {}
+  constructor(private shoppingCartService: ShoppingCartService) {}
 
   async ngOnInit() {
-    const cart$ = await this.shoppingCartService.getCart();
-    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
-    this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
-  }
-
-  ngOnDestroy() {
-    this.cartSubscription.unsubscribe();
-    this.userSubscription.unsubscribe();
-  }
-
-  async placeOrder() {
-    const order = new Order(this.userId, this.shipping, this.cart);
-    const result = await this.orderService.placeOrder(order);
-    this.router.navigate(['/order-success', result.key]);
+    this.cart$ = await this.shoppingCartService.getCart();
   }
 
 }
